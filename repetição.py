714f9f5269959ab355a11a380df7b4f48ea0bf3c
luisotavio336 <filhotavinho85@gmail.com>
2026-03-03 08:37:51 -0300

    Add files via upload

---
+++ repetição.py
@@ -0,0 +1,12 @@
+ cont = 1
+while cont <=10:
+    print("Olá, Mundo")
+    cont += 1 # cont = cont + 1
+print("FIM")
+
+print(list(range(5)))
+print(list(range(1,10,2)))
+print(list(range(1,10)))
+
+for cont in range(10):
+    print("Olá, mundo!")
