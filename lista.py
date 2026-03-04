919548ad4410a6cfec3604c695ea9e64b029c50e
luisotavio336 <filhotavinho85@gmail.com>
2026-03-04 08:39:17 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,30 @@
+#listas 
+
+nome1 = "ANA"
+nome2 = "BRUNO"
+nome3  = "CARLOS"
+
+nomes = ["ana", "Bruno","Carlos"]
+print(nomes)
+
+dados["Gabriel", 0, 1.60, True]
+
+print(dados)
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+alunos =["ana", "bruno", "carlos"]
+
+print("primeiro", alunos[0])
+print("primeiro", alunos[1])
+print("primeiro", len(alunos))
+print("primeiro", alunos[2])
+print("primeiro", alunos[len(alunos) -1])
+
+nomes = ["Ana", "bruno", "carlos"]
+print("original",nomes)
+nomes.append("daniel")
+print("atualizado:,"nomes)
