e9426c93a785f78dff036cb17c6d701637f21a6c
luisotavio336 <filhotavinho85@gmail.com>
2026-04-08 07:57:30 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,13 @@
+P = int(input())
+D = int(input())
+B = int(intup())
+
+total = P + 2*D + 3*B
+if total >= 150:
+   print("B")
+elif total >= 120:
+   print("D")
+elif total >= 100:
+   print("P")
+else:
+   print("N")   
